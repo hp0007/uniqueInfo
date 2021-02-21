c6c95f72bc569953519273c72890418d88c3e8eb
hp0007 <harshitgajjar95336@gmail.com>
2021-02-22 00:59:08 +0530

    error fix

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ import Card from '../components/Card'
 const Cards = (props) => {
     
     return (  
-        <div className='row pb-5'>
+        <div className='row '>
             {
                 props.datafirst.map(data => {
                     return(
